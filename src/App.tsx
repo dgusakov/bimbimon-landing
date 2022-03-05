41015f1779bee13f6cd6da06202ef45a7f2707be
Dmitry Gusakov <dmitry.gusakov@p2p.org>
2022-03-05 13:19:20 +0300

    Move to TS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./components/Header"
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import "../App.css";
+import logo from '../logo.svg';
+import { Navbar } from "react-bootstrap";
+import React from "react";
+
+function Header() {
+  return (
+    <header className="App-header">
+      <Navbar className="mt-0">
+        <div className="container justify-content-center">
+          <Navbar.Brand href="/" className="text-center m-0">
+            <img
+              alt=""
+              src={logo}
+              className="d-inline-block App-logo-header m-0"
+            />
+          </Navbar.Brand>
+        </div>
+      </Navbar>
+    </header>
+  );
+}
+
+export default Header;
