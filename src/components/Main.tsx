b90bf69ece611bc4e520c4045016fa7553089e5b
Dmitry Gusakov <dmitry.gusakov@p2p.org>
2022-03-15 15:34:24 +0300

    Move VK to the bottom on main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -23,11 +23,11 @@ function Main() {
         <ListButtonWithIcon displayName={["Мы в ", <strong key="1">Instagam</strong>]} link="https://www.instagram.com/bimbimon_tm/" key="inst">
             <InstagramLogo className="Social-logo-inline" />
         </ListButtonWithIcon>
-        <ListButtonWithIcon displayName={["Всегда ", <strong key="1">ВКонатакте</strong>]} link="https://vk.com/club210948125" key="vk">
-            <VKLogo className="Social-logo-inline" />
-        </ListButtonWithIcon>
         <ListButtonWithIcon displayName="Познавательные ролики" link="https://www.youtube.com/channel/UCKziH5v62PIeqmjxzZo0WFA/featured" key="youtube">
             <YouTubeLogo className="Social-logo-inline" />
+        </ListButtonWithIcon>
+        <ListButtonWithIcon displayName={["Всегда ", <strong key="1">ВКонатакте</strong>]} link="https://vk.com/club210948125" key="vk">
+            <VKLogo className="Social-logo-inline" />
         </ListButtonWithIcon>
       </div>
     </div>
