6433185bf1265a639f189d63aa0a4a2f8c7867e0
Dmitry Gusakov <dmitry.gusakov@p2p.org>
2022-03-15 15:33:27 +0300

    Add VK links to main and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,7 @@ import "../css//App.css";
 import { ReactComponent as InstagramLogo } from '../img/instagram.svg';
 import { ReactComponent as YouTubeLogo } from "../img/youtube.svg";
 import { ReactComponent as Cart } from "../img/cart.svg";
+import { ReactComponent as VKLogo } from '../img/vk.svg';
 import React from "react";
 
 const year = new Date().getFullYear();
@@ -12,17 +13,22 @@ function Footer() {
     <div className="container">
       <div className="row justify-content-center mb-2">
         <ul className="nav col justify-content-center">
-          <li className="nav-item px-sm-5 pe-2">
+          <li className="nav-item px-sm-4 pe-1">
             <a className="text-muted p-3" href="https://www.youtube.com/channel/UCKziH5v62PIeqmjxzZo0WFA/featured">
               <YouTubeLogo className="Social-logo" />
             </a>
           </li>
-          <li className="nav-item px-sm-5 px-2">
+          <li className="nav-item px-sm-4 px-1">
             <a className="text-muted p-3" href="https://www.instagram.com/bimbimon_tm/">
               <InstagramLogo className="Social-logo"/>
             </a>
           </li>
-          <li className="nav-item px-sm-5 ps-2">
+          <li className="nav-item px-sm-4 px-1">
+            <a className="text-muted p-3" href="https://vk.com/club210948125">
+              <VKLogo className="Social-logo"/>
+            </a>
+          </li>
+          <li className="nav-item px-sm-4 ps-1">
             <a className="text-muted p-3" href="https://taplink.cc/bimbimon_tm">
               <Cart className="Social-logo"/>
             </a>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,6 +6,7 @@ import ListButtonWithIcon from "./navs/ListButtonWithIcon";
 import { ReactComponent as InstagramLogo } from '../img/instagram.svg';
 import { ReactComponent as YouTubeLogo } from "../img/youtube.svg";
 import { ReactComponent as Cart } from "../img/cart.svg";
+import { ReactComponent as VKLogo } from "../img/vk.svg";
 
 function Main() {
   return (
@@ -21,6 +22,9 @@ function Main() {
         </ListButtonWithIcon>
         <ListButtonWithIcon displayName={["Мы в ", <strong key="1">Instagam</strong>]} link="https://www.instagram.com/bimbimon_tm/" key="inst">
             <InstagramLogo className="Social-logo-inline" />
+        </ListButtonWithIcon>
+        <ListButtonWithIcon displayName={["Всегда ", <strong key="1">ВКонатакте</strong>]} link="https://vk.com/club210948125" key="vk">
+            <VKLogo className="Social-logo-inline" />
         </ListButtonWithIcon>
         <ListButtonWithIcon displayName="Познавательные ролики" link="https://www.youtube.com/channel/UCKziH5v62PIeqmjxzZo0WFA/featured" key="youtube">
             <YouTubeLogo className="Social-logo-inline" />
