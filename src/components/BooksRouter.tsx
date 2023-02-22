c1ac50fce7516baa499ef7db19c2813394494962
Dmitry Gusakov <gusakov.dv@gmail.com>
2023-02-22 15:21:24 +0400

    fix: Add missing files

---
+++ src/components/BooksRouter.tsx
@@ -0,0 +1,24 @@
+import "../css//App.css";
+import { getBookLink } from "../data/books";
+import React from "react";
+import { useParams } from "react-router-dom";
+import NotFound from "./NotFound";
+import HeaderBoldCenter from "./labels/HeaderBoldCenter";
+
+function BooksRouter(props: any) {
+  let urlParams = useParams();
+  try {
+    const link = getBookLink(urlParams.id || "");
+    window.location.href = link;
+    return (
+      <div className="container mt-3">
+        <HeaderBoldCenter name="Подготавливаем для вас книгу! Еще пару секунд..." />
+      </div>
+    );
+  } catch (error) {
+    console.error(error);
+    return <NotFound />;
+  }
+}
+
+export default BooksRouter;
---
+++ src/data/books.ts
@@ -0,0 +1,11 @@
+export const books = new Map<string, string>([
+  ["puss-in-boots", "https://youtu.be/CjZ7j71kpvM"],
+]);
+
+export function getBookLink(name: string) {
+  const link = books.get(name);
+  if (link) {
+    return link;
+  }
+  throw new Error(`Book ${name} not found`);
+}
