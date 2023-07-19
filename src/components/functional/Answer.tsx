d4475ad081df21c87b841c133e74e98f07112dfc
Ivan Gusakov <74269317+larmork@users.noreply.github.com>
2023-07-19 20:52:42 +0300

    Fix problem with shown answer

--- src/components/functional/Answer.tsx
+++ src/components/functional/Answer.tsx
@@ -1,12 +1,19 @@
 import "../../css/App.css";
-import React from "react";
+import React, { useState } from "react";
 import HeaderLight from "../labels/HeaderLight";
 
 function Answer(props: any) {
-  const [showAnswer, setShowAnswer] = React.useState(false);
-  const showAnswerFunc = () => setShowAnswer(true);
+  // State to track whether the answer is visible or hidden
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  // Function to toggle the visibility of the answer
+  const toggleAnswer = () => {
+    setShowAnswer((prevShowAnswer) => !prevShowAnswer);
+  };
+
   return (
     <div>
+      {/* The answer section, hidden by default if showAnswer is false */}
       <div id="answer" style={{ display: showAnswer ? "block" : "none" }}>
         <HeaderLight name="Ответ" />
         <div className="mt-3" id="question">
@@ -14,14 +21,25 @@ function Answer(props: any) {
         </div>
       </div>
       <div className="d-grid gap-2 mb-2 mt-4">
+        {/* Button to show the answer when clicked, visible if showAnswer is false */}
         <button
           id="showAnswer"
           className="btn btn-main"
           type="button"
-          onClick={showAnswerFunc}
+          onClick={toggleAnswer}
           style={{ display: showAnswer ? "none" : "block" }}
         >
           Узнать ответ
+        </button>
+        {/* Button to hide the answer when clicked, visible if showAnswer is true */}
+        <button
+          id="hideAnswer"
+          className="btn btn-main"
+          type="button"
+          onClick={toggleAnswer}
+          style={{ display: showAnswer ? "block" : "none" }}
+        >
+          Скрыть ответ
         </button>
       </div>
     </div>
