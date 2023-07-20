d434d44759ab8117d5c3c7b9803b4165cd55a8c9
Ivan Gusakov <74269317+larmork@users.noreply.github.com>
2023-07-20 19:37:47 +0300

    Components Block and BigBlock

--- src/components/blockchain/Block.tsx
+++ src/components/blockchain/Block.tsx
@@ -1,18 +1,53 @@
 import "../../css/App.css";
+import { useState } from "react";
 import { BLOCK_GROUPS } from '../../data/blockchain';
 
+// Block component
 function Block(props: any) {
-  const group = Number(props.group) || 0
-  const id = Number(props.id) - 1 || 0
-  const BlockIcon = BLOCK_GROUPS[group][id]
+  // Get the group and id props from the parent component
+  const group = Number(props.group) || 0;
+  const id = Number(props.id) - 1 || 0;
 
-  let className = props.className
-  if (props.activated) {
-    className+= " activated inactive"
-  }
+  // Get the corresponding BlockIcon component based on the group and id
+  const BlockIcon = BLOCK_GROUPS[group][id];
+
+  // State to track the activation status of the block
+  const [activated, setActivated] = useState(false);
+
+  // Function to toggle the activation state when the block is clicked
+  const toggleActivation = () => {
+    setActivated(!activated);
+    // Call the onClick prop passed from the parent component
+    props.onClick();
+  };
+
+  // Determine the class name based on the activation status
+  const className = activated ? "block-icon activated" : "block";
+
+  // Render the Block component with the BlockIcon and onClick handler
   return (
-    <BlockIcon className={className} onClick={props.onClick} id={props.id}/>
+    <div className="block" onClick={toggleActivation}>
+      <BlockIcon className={className} onClick={props.onClick} id={props.id} />
+    </div>
   );
 }
+
+// Props for the BigBlock component
+interface BigBlockProps {
+  group: number;
+}
+
+// BigBlock component
+export const BigBlock: React.FC<BigBlockProps> = ({ group }) => {
+  // Get the corresponding BigBlockIcon component based on the group
+  const BigBlockIcon = BLOCK_GROUPS[group][4];
+
+  // Render the BigBlock component with the BigBlockIcon
+  return (
+    <div className="big-block-container">
+      <BigBlockIcon className="big-block-icon" />
+    </div>
+  );
+};
 
 export default Block;
